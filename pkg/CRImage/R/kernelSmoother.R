a8f4717ffd70284a5eb9c936ff88abc7292004a8
henrik_86 (Henrik Failmezger) <Henrik.Failmezger@googlemail.com>
2010-07-08 10:21:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/crimage@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CRImage/R/kernelSmoother.R
@@ -0,0 +1,70 @@
+`kernelSmoother` <-
+function(predictedClasses,cellCoordinates,segmentedImage,cancerIdentifier,indexCells){
+				#options(stringsAsFactors = FALSE)
+				prob=attr(predictedClasses, "prob")
+				probCancer=prob[,cancerIdentifier]
+				print("prob cancer")
+				cellCoordinatesN=cellCoordinates
+				cellCoordinatesN=cbind(indexCells,cellCoordinates)
+				cellCoordinatesN=cbind(cellCoordinatesN,1:dim(cellCoordinates)[1])
+				imgW=dim(segmentedImage)[1]
+				imgH=dim(segmentedImage)[2]
+				xs=imgW/2
+				xPos=imgW/2
+				ys=imgH/2
+				yPos=imgH/2
+				allNewClasses=data.frame()
+				windowCounter=0
+				for (iW in 1:2){
+					xPos=xs
+					for (jW in 1:2){
+						if(length(cellCoordinatesN)>0){
+						actualCoordinates=subset(cellCoordinatesN,cellCoordinatesN[,2]<=xPos & cellCoordinatesN[,3]<=yPos)
+						indexActualCoordinates=which(cellCoordinatesN[,2]<=xPos & cellCoordinatesN[,3]<=yPos)
+						cellCoordinatesN=subset(cellCoordinatesN, !(cellCoordinatesN[,4] %in% indexActualCoordinates))
+						cellCoordinatesN[,4]=1:dim(cellCoordinatesN)[1]
+						if(dim(actualCoordinates)[1]>0){
+							windowCounter=windowCounter+1
+							distMatrix=as.matrix(dist(actualCoordinates[,2:3]))
+							
+							f=c()
+							for (i in 1:dim(actualCoordinates)[1]){
+								probCancerW=probCancer[actualCoordinates[,"indexCells"]]
+								t=distMatrix[i,]/50
+								indexZero=which(t>1)
+								k=(1-t^3)^3
+								k[indexZero]=0
+								f=c(f,sum(k*probCancerW)/sum(k))
+							}
+							index=actualCoordinates[,"indexCells"]
+							classValues=f
+							newClassesKS=data.frame(index,classValues)
+							if(windowCounter==1){
+								allNewClasses=newClassesKS
+							}else{
+								allNewClasses=rbind(allNewClasses,newClassesKS)
+							}
+						}
+						}
+						xPos=xPos+xs
+					}
+					yPos=yPos+ys
+				}
+			
+	
+			allNewClasses=as.data.frame(allNewClasses)
+			allNewClasses$classValues[allNewClasses$classValues<0.5]=0
+			allNewClasses$classValues[allNewClasses$classValues>=0.5]=2
+			allNewClasses$classValues[allNewClasses$classValues==0]=1
+			allNewClasses$classValues[allNewClasses$classValues==1]="o"
+			allNewClasses$classValues[allNewClasses$classValues==2]=cancerIdentifier
+			
+			realIndex=data.frame(indexCells)
+			colnames(realIndex)="index"
+			colnames(allNewClasses)=c("index","classValues")
+			classesRealIndices=merge(realIndex,allNewClasses,all.x=TRUE,all.y=TRUE)
+			allClasses=classesRealIndices[,"classValues"]
+			classSVM=allClasses
+			classSVM
+}
+
